0daf2a53fe22452643aa3742c891fd1b90bee5f3
Marek Caputa <me@mark97.net>
2017-03-27 23:03:32 +0200

    Resolve conflicts

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,29 @@
 class Admin::UsersController < Admin::BaseController
+  expose :user
+  expose :interests, from: :user
+  expose_decorated(:users, -> { User.all })
+
+  def create
+    user.password = 'secret'
+
+    if user.save
+      redirect_to [:admin, :users], notice: t('.success', email: user.email)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if user.update_attributes(user_params)
+      redirect_to [:admin, :users], notice: t('.success', email: user.email)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :age, :gender, interests_attributes: [:id, :name, :interest_type, :_destroy])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   resources :users, only: %i(index destroy)
 
   namespace :admin do
-    resources :users, except: :show
+    resources :users, except: %i(show destroy)
   end
 end
