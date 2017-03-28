abfb6113074e9d9eb764ac12e92fe2166415f78f
Marek Caputa <me@mark97.net>
2017-03-28 18:13:11 +0200

    Add amount of points of interests to admin panel

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -4,4 +4,7 @@ class Interest < ApplicationRecord
   belongs_to :user
 
   validates :name, :interest_type, presence: true
+
+  scope :health_type, -> { where(interest_type: :health) }
+  scope :specific_scope, -> { health_type.joins(:user).where(user_id: User.young_women.select(:id)).where('name like ?', 'cosm%') }
 end
